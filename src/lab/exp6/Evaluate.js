5331df36ef85b6a7887715bd15bc3b8ac4f0eb89
Mani082 <41281962+Mani082@users.noreply.github.com>
2018-10-29 00:37:05 +0530

    Added Evaluate.js file which evaluates the quiz

---
+++ src/lab/exp6/Evaluate.js
@@ -0,0 +1,21 @@
+function check(){
+	var Q1=document.quiz.q1.value;
+	var Q2=document.quiz.q2.value;
+	var Q3=document.quiz.q3.value;
+	var Q4=document.quiz.q4.value;
+
+	var c=0;
+	if(Q1=="1"){
+		c++;
+	}
+	if (Q2=="0") {
+		c++;
+	}
+	if(Q3=="1"){
+	    c++;
+	}
+	if(Q4=="0"){
+	    c++;
+	}
+	window.alert("You got "+c+" correct!")
+}
