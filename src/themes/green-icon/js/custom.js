3680c6fed41c05be9dbf496e20d94c6cb889d967
DevikaBoddu <40534152+DevikaBoddu@users.noreply.github.com>
2018-11-17 23:28:09 +0530

    Update custom.js

--- src/themes/green-icon/js/custom.js
+++ src/themes/green-icon/js/custom.js
@@ -6,10 +6,10 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('body').on('click', '.page-scroll a', function(event) {
+    $('body').on("click", ".page-scroll a", function(event) {
         var $anchor = $(this);
         $("html, body").stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr("href")).offset().top
         }, 1500, "easeInOutExpo");
         event.preventDefault();
     });
