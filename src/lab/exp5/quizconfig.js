5bc4a3a6feab11e023d58544414ea6c4914a4551
DevikaBoddu <40534152+DevikaBoddu@users.noreply.github.com>
2018-11-17 23:35:29 +0530

    Update quizconfig.js

--- src/lab/exp5/quizconfig.js
+++ src/lab/exp5/quizconfig.js
@@ -19,25 +19,25 @@ var incorrect=null
 for (q=1;q<=totalquestions;q++){
 	var thequestion=eval("document.myquiz.question"+q)
 	for (c=0;c<thequestion.length;c++){
-		if (thequestion[c].checked===true)
-		actualchoices[q]=thequestion[c].value
+		if (thequestion[c].checked===true){
+		actualchoices[q]=thequestion[c].value}
 		}
 		
 	if (actualchoices[q]!==correctchoices[q]){ //process an incorrect choice
-		if (incorrect==null)
-		incorrect=q
-		else
-		incorrect+="/"+q
+		if (incorrect==null){
+		incorrect=q}
+		else{
+		incorrect+="/"+q}
 		}
 	}
 
 if (incorrect==null)
 incorrect="a/b"
 document.cookie="q="+incorrect
-if (document.cookie==="")
-alert("Your browser does not accept cookies. Please adjust your browser settings.")
-else
-window.location="results.htm"
+if (document.cookie===""){
+alert("Your browser does not accept cookies. Please adjust your browser settings.")}
+else{
+window.location="results.htm"}
 }
 
 
