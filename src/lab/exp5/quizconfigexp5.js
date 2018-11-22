520b2148f42330fc51412f1a248414a852936e98
ShivaniKomandury <44065594+ShivaniKomandury@users.noreply.github.com>
2018-11-22 10:44:18 +0530

    Exp 5 Update quizconfig.js

--- src/lab/exp5/quizconfigexp5.js
+++ src/lab/exp5/quizconfigexp5.js
@@ -6,20 +6,25 @@
 ***********************************************/
 
 //Enter total number of questions:
-var totalquestions=1
-var q
+var totalquestions=1;
+var q=1;
+var c=0;
+var i=1;
+var temp=0;
+var incorrect=null;
+var actualchoices=new Array(200);
 //Enter the solutions corresponding to each question:
-var correctchoices=new Array()
+var correctchoices=new Array();
 correctchoices[1]='b' //question 1 solution
 
 /////Don't edit beyond here//////////////////////////
 
 function gradeit(){
-var incorrect=null
+
 for (q=1;q<=totalquestions;q++){
 	var thequestion=eval("document.myquiz.question"+q)
 	for (c=0;c<thequestion.length;c++){
-		if (thequestion[c].checked==true){
+		if (thequestion[c].checked===true){
 		actualchoices[q]=thequestion[c].value
 		}
 	}
@@ -34,17 +39,18 @@ for (q=1;q<=totalquestions;q++){
 	}
 	}
 
-if (incorrect==null){
+if (incorrect===null){
 incorrect="a/b"
 }
 document.cookie='q='+incorrect
-if (document.cookie==''){
+if (document.cookie===""){
 alert("Your browser does not accept cookies. Please adjust your browser settings.")
 }
-else{
+	else{
 window.location="results.htm"
 }
 }
+
 
 function showsolution(){
 var win2=window.open("","win2","width=200,height=350, scrollbars")
@@ -56,7 +62,7 @@ win2.document.write('<center><h3>Solution to Quiz</h3></center>')
 win2.document.write('<center><font face="Arial">')
 for (i=1;i<=totalquestions;i++){
 for (temp=0;temp<incorrect.length;temp++){
-if (i==incorrect[temp]){
+if (i===incorrect[temp]){
 wrong=1
 }
 }
