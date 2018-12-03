cce5478e70f3487438a726cb2335b46662970f9c
Somashekhar31 <17311a05r5@sreenidhi.edu.in>
2018-12-03 18:13:19 +0530

    changes made1

--- src/themes/green-icon/js/custom.js
+++ src/themes/green-icon/js/custom.js
@@ -6,11 +6,11 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('body').on('click', '.page-scroll a', function(event) {
+    $("body").on("click", ".page-scroll a", function(event) {
         var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        $("html, body").stop().animate({
+            scrollTop: $($anchor.attr("href")).offset().top
+        }, 1500, "easeInOutExpo");
         event.preventDefault();
     });
 });
@@ -27,11 +27,11 @@ $(function() {
 });
 
 // Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
+$("body").scrollspy({
+    target: ".navbar-fixed-top"
 })
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+$(".navbar-collapse ul li a").click(function() {
+    $(".navbar-toggle:visible").click();
 });
