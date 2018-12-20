7f765626e58d81bcb5330934de2feb20133b078c
Somashekhar31 <17311a05r5@sreenidhi.edu.in>
2018-12-20 19:01:33 +0530

    resolve 1

--- src/lab/exp5/quizconfig.js
+++ src/lab/exp5/quizconfig.js
@@ -10,7 +10,7 @@ var totalquestions=1
 
 //Enter the solutions corresponding to each question:
 var correctchoices=new Array()
-correctchoices[1]='b' //question 1 solution
+correctchoices[1]="b"; //question 1 solution
 
 /////Don't edit beyond here//////////////////////////
 
@@ -19,8 +19,9 @@ var incorrect=null
 for (q=1;q<=totalquestions;q++){
 	var thequestion=eval("document.myquiz.question"+q)
 	for (c=0;c<thequestion.length;c++){
-		if (thequestion[c].checked===true)
+		if (thequestion[c].checked===true){
 		actualchoices[q]=thequestion[c].value
+		}
 		}
 		
 	if (actualchoices[q]!==correctchoices[q]){ //process an incorrect choice
@@ -45,23 +46,23 @@ function showsolution(){
 var win2=window.open("","win2","width=200,height=350, scrollbars")
 win2.focus()
 win2.document.open()
-win2.document.write('<title>Solution</title>')
-win2.document.write('<body bgcolor="#FFFFFF">')
-win2.document.write('<center><h3>Solution to Quiz</h3></center>')
-win2.document.write('<center><font face="Arial">')
-for (i=1;i<=totalquestions;i++){
+win2.document.write("<title>Solution</title>");
+win2.document.write("<body bgcolor="#FFFFFF">");
+win2.document.write("<center><h3>Solution to Quiz</h3></center>");
+win2.document.write("<center><font face="Arial">");
+for (var i=1;i<=totalquestions;i++){
 for (temp=0;temp<incorrect.length;temp++){
-if (i==incorrect[temp])
-wrong=1
+if (i===incorrect[temp])
+var wrong=1;
 }
-if (wrong==1){
-win2.document.write("Question "+i+"="+correctchoices[i].fontcolor("red")+"<br>")
-wrong=0
+if (wrong===1){
+win2.document.write("Question "+i+"="+correctchoices[i].fontcolor("red")+"<br>");
+var wrong=0;
 }
 else
-win2.document.write("Question "+i+"="+correctchoices[i]+"<br>")
+win2.document.write("Question "+i+"="+correctchoices[i]+"<br>");
 }
-win2.document.write('</center></font>')
+win2.document.write("</center></font>");
 win2.document.write("<h5>Note: The solutions in red are the ones to the questions you had incorrectly answered.</h5>")
 win2.document.close()
 }
