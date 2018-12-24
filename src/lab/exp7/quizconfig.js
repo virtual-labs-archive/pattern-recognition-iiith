d45515c31c0f4de2cfc71be210694b693e11d9aa
VinodRathod2209 <44551454+VinodRathod2209@users.noreply.github.com>
2018-12-24 22:41:16 +0530

    Updated exp7quizconfig.js
    Removed function complexity

--- src/lab/exp7/quizconfig.js
+++ src/lab/exp7/quizconfig.js
@@ -15,14 +15,14 @@ correctchoices[2]='b' //question 2 solution, and so on.
 
 
 /////Don't edit beyond here//////////////////////////
-
-function gradeit(){
-var incorrect=null
-for (q=1;q<=totalquestions;q++){
+function checkQuestions(incorrect)
+{
+	var q,c
+	for (q=1;q<=totalquestions;q++){
 	var thequestion=document.myquiz.question[q]
 	for (c=0;c<thequestion.length;c++){
 		if (thequestion[c].checked===true)
-		actualchoices[q]=thequestion[c].value
+			actualchoices[q]=thequestion[c].value
 		}
 		
 	if (actualchoices[q]!==correctchoices[q]){ //process an incorrect choice
@@ -32,17 +32,43 @@ for (q=1;q<=totalquestions;q++){
 		incorrect+="/"+q
 		}
 	}
+	return incorrect;
 
+};
+
+function checkCookies()
+{
+	document.cookie='q='+incorrect
+	if (document.cookie==='')
+		alert("Your browser does not accept cookies. Please adjust your browser settings.")
+	else
+		window.location="results.htm"
+};
+
+function gradeit(){
+var incorrect=null
+incorrect=checkQuestions(incorrect);
 if (incorrect==null)
 incorrect="a/b"
-document.cookie='q='+incorrect
-if (document.cookie==='')
-alert("Your browser does not accept cookies. Please adjust your browser settings.")
-else
-window.location="results.htm"
+checkCookies();
 }
 
-
+function checkWin2(win2)
+{
+	var i,temp,wrong;
+	for (i=1;i<=totalquestions;i++){
+		for (temp=0;temp<incorrect.length;temp++){
+			if (i==incorrect[temp])
+				wrong=1
+		}
+		if (wrong==1){
+			win2.document.write("Question "+i+"="+correctchoices[i].fontcolor("red")+"<br>")
+			wrong=0
+		}
+		else
+			win2.document.write("Question "+i+"="+correctchoices[i]+"<br>")
+	}
+}
 function showsolution(){
 var win2=window.open("","win2","width=200,height=350, scrollbars")
 win2.focus()
@@ -51,18 +77,7 @@ win2.document.write('<title>Solution</title>')
 win2.document.write('<body bgcolor="#FFFFFF">')
 win2.document.write('<center><h3>Solution to Quiz</h3></center>')
 win2.document.write('<center><font face="Arial">')
-for (i=1;i<=totalquestions;i++){
-for (temp=0;temp<incorrect.length;temp++){
-if (i==incorrect[temp])
-wrong=1
-}
-if (wrong==1){
-win2.document.write("Question "+i+"="+correctchoices[i].fontcolor("red")+"<br>")
-wrong=0
-}
-else
-win2.document.write("Question "+i+"="+correctchoices[i]+"<br>")
-}
+checkWin2(win2);
 win2.document.write('</center></font>')
 win2.document.write("<h5>Note: The solutions in red are the ones to the questions you had incorrectly answered.</h5>")
 win2.document.close()
