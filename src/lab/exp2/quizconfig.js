e83ad49154d98ea215a71c4f41e1d89e0a9323bc
VinodRathod2209 <44551454+VinodRathod2209@users.noreply.github.com>
2018-12-26 11:59:52 +0530

    Updated exp2quizconfig.js

--- src/lab/exp2/quizconfig.js
+++ src/lab/exp2/quizconfig.js
@@ -7,7 +7,8 @@
 
 //Enter total number of questions:
 var totalquestions=5
-
+var incorrect={};
+var actualchoices={};
 //Enter the solutions corresponding to each question:
 var correctchoices=new Array()
 correctchoices[1]='a' //question 1 solution
@@ -18,6 +19,21 @@ correctchoices[5]='e'
 
 
 /////Don't edit beyond here//////////////////////////
+function checkChoice(incorrect,q,actualchoices)
+{
+	if (actualchoices[q]!==correctchoices[q])
+	{ //process an incorrect choice
+		if (incorrect==null)
+		{
+	 		incorrect=q;
+		}
+		else
+		{
+			incorrect+="/"+q;
+		}
+	}
+	return incorrect;
+}
 function questionCheck(incorrect)
 {
 	var q,c;
@@ -28,58 +44,60 @@ function questionCheck(incorrect)
 			actualchoices[q]=thequestion[c].value
 		}
 	}
-	if (actualchoices[q]!==correctchoices[q]){ //process an incorrect choice
-		if (incorrect==null)
-			 incorrect=q
-		else
-			incorrect+="/"+q
-		}
-	}
+	incorrect=checkChoice(incorrect,q,actualchoices);
 	return incorrect;
 };
-function checkCookie()
+function checkCookie(incorrect)
 {
-	document.cookie='q='+incorrect
-	if (document.cookie==='')
+	document.cookie="q="+incorrect;
+	if (document.cookie==="")
+	{
 		alert("Your browser does not accept cookies. Please adjust your browser settings.")
-	else
+	}
+	else{
 		window.location="results.htm"
+	}
 }
 function gradeit(){
-	var incorrect=null
+	incorrect=null
 	incorrect=questionCheck(incorrect);
 	if (incorrect==null)
 		incorrect="a/b"
 	checkCookie();
 };
-
-function checkSolution(win2)
+function checkWrong(wrong)
 {
-	var temp,wrong,i;
+	if (wrong===1){
+			win2.document.write("Question "+i+"="+correctchoices[i].fontcolor("red")+"<br>");
+			wrong=0;
+		}
+		else
+			win2.document.write("Question "+i+"="+correctchoices[i]+"<br>");	
+}
+function checkSolution(win2,temp)
+{
+	
+	var wrong,i;
 	for (i=1;i<=totalquestions;i++){
 		for (temp=0;temp<incorrect.length;temp++){
 			if (i===incorrect[temp]){
 				wrong=1;
 			}
 		}
-		if (wrong===1){
-			win2.document.write("Question "+i+"="+correctchoices[i].fontcolor("red")+"<br>");
-			wrong=0;
-		}
-		else
-			win2.document.write("Question "+i+"="+correctchoices[i]+"<br>");
+		wrong=checkWrong(wrong);
 	}
 };
 function showsolution(){
 var win2=window.open("","win2","width=200,height=350, scrollbars")
+var temp=0;
 win2.focus()
 win2.document.open()
-win2.document.write('<title>Solution</title>')
-win2.document.write('<body bgcolor="#FFFFFF">')
-win2.document.write('<center><h3>Solution to Quiz</h3></center>')
+win2.document.write("<title>Solution</title>")
+win2.document.write("<body bgcolor="#FFFFFF">")
+win2.document.write("<center><h3>Solution to Quiz</h3></center>")
 win2.document.write('<center><font face="Arial">')
-checkSolution(win2);
-win2.document.write('</center></font>')
+checkSolution(win2,temp);
+win2.document.write("</center></font>")
 win2.document.write("<h5>Note: The solutions in red are the ones to the questions you had incorrectly answered.</h5>")
 win2.document.close()
 }
