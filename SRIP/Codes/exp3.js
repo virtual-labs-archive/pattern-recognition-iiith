90462b39d71b30d9ae38fb5ba836921611f74657
prekshap24 <rush2preksha@gmail.com>
2019-05-29 13:09:19 +0530

    Perceptron algorithm completed

--- SRIP/Codes/exp3.js
+++ SRIP/Codes/exp3.js
@@ -1,16 +1,18 @@
 var xaxis = [-1.0 ,-0.75, -0.50, -0.25, 0.0, 0.25, 0.50, 0.75, 1.0];
 var finaldata = [];  //dataset for Perceptron
-var data1=[]; //for class 1
-var count1=0; //count of all points of class 1
-var data2=[]; //for class 2
-var count2=0; //count of all points of class 2
+var chart1data = [];
+var chart2data = [];
+var data1 = []; //for class 1
+var count1 = 0; //count of all points of class 1
+var data2 = []; //for class 2
+var count2 = 0; //count of all points of class 2
 var learningParameter;
 var dataArray = [];
-var q1=0;
-var q2=0;
-var weights=[0, 0];
-var bias=1;
-var c=0, d=0;
+var q1 = 0;
+var q2 = 0;
+var weights = [0, 0];
+var bias = 1;
+var c = 0, d = 0;
 var activation = 0;
 var k = 0.05;
 
@@ -31,16 +33,23 @@ function addDataPoints1x() {
 	var xValue1;
     xValue1 = Number(document.getElementById("xValue1").value);
     data1[q1] = xValue1;
+    chart1data.push({
+    	x: xValue1
+    });
     q1++;
     count1++;
     console.log(data1); //Ctrl+Shift+J
 	return data1; //seeing the output in console window
 }
 
+
 function addDataPoints1y() {
 	var yValue1;
 	yValue1 = Number(document.getElementById("yValue1").value);
     data1[q1] = yValue1;
+    chart1data.push({
+    	y: yValue1
+    });
     q1++;
     count1++;
    	console.log(data1); 
@@ -53,16 +62,23 @@ function addDataPoints2x() {
 	var xValue2;
     xValue2 = Number(document.getElementById("xValue2").value);
     data2[q2] = xValue2;
+    chart2data.push({
+    	x: xValue2
+    });
     q2++;
     count2++;
     console.log(data2); //Ctrl+Shift+J
 	return data2; //seeing the output in console window
 }
 
+
 function addDataPoints2y() {
 	var yValue2;
 	yValue2 = Number(document.getElementById("yValue2").value);
     data2[q2] = yValue2;
+    chart2data.push({
+    	y: yValue2
+    });
     q2++;
     count2++;
    	console.log(data2); 
@@ -117,20 +133,15 @@ function addClass2(){
 /*Creating Graph 2*/  
 var ctx = document.getElementById("graph-2");
 var myChart2 = new Chart(ctx, {
-	type: "scatter",
-  	data: {
-  	labels: xaxis, 
-  	dataset: []
-  	}
   });
 
 
 //Perceptron function
 function predict(c, weights){
-	for(d=0; d<2; d++){
+	for(d = 0; d < 2; d++){
 		activation = activation + (weights[d] * dataArray[c][d]) + bias;
 	}
-	if(activation>=0.0){
+	if(activation >= 0.0){
 		return 1.0;
 	}
 	else{
@@ -138,19 +149,21 @@ function predict(c, weights){
 	}
 
 }
+
+
 function perceptronTrainWeights(dataArray, learningParameter){
-	var j=0;
+	var j = 0;
 	var sumError = 0;
-	var error = 0;
+	var error = 1;
 	var n_epoch = 5;
 	var prediction = 0;
-	for(var epoch=0; epoch<n_epoch; epoch++){
+	while(error>0){
 		sumError = 0;
-		for(c=0; c<dataArray.length; c++) {
+		for(c = 0; c < dataArray.length; c++) {
 			prediction = predict(c, weights);
-			error=dataArray[c][2] - prediction;
-			sumError += error*2;
-			for(j=0; j<2; j++){
+			error = dataArray[c][2] - prediction;
+			sumError += error * 2;
+			for(j = 0; j < 2; j++){
 				weights[j] = weights[j] + (learningParameter * error * dataArray[c][j]);
 			}
 		}	
@@ -162,47 +175,64 @@ function perceptronTrainWeights(dataArray, learningParameter){
 /*to run the Perceptron Algorithm
  On clicking the Start button*/
 function start(){
-	document.getElementById("step").style.visibility="visible";
-	document.getElementById("step-100").style.visibility="visible";
+	document.getElementById("step").style.visibility = "visible";
+	document.getElementById("step-100").style.visibility = "visible";
 	learningParameter = document.getElementById("learning-parameter").value;
 	
-	var i=0;
+	var i = 0;
 
-	for(i=0; i<(count1+count2); i++){
-		if(i<count1)
+	for(i = 0; i < (count1 + count2); i++){
+		if(i < count1)
 			finaldata[i] = data1[i];
 		else
-			finaldata[i] = data2[i-count1];
+			finaldata[i] = data2[i - count1];
 	}
 
-	
-
-	var conut=0;
+	var conut = 0;
 	//2D final array
-	for(i=0; i<count1/2; i++){
+	for(i = 0; i < count1 / 2; i++){
 		dataArray[i] = [];
 		dataArray[i][0] = finaldata[conut];
 		conut++;
 		dataArray[i][1] = finaldata[conut];
-		conut++
+		conut++;
 		dataArray[i][2] = 1;
 	}
 
-	i=count1/2;
-	var a=0;
-	while(a!=count2/2){
+	i = count1 / 2;
+	var a = 0;
+	while(a != count2 / 2){
 		dataArray[i] = [];
 		dataArray[i][0] = finaldata[conut];
 		conut++;
 		dataArray[i][1] = finaldata[conut];
-		conut++
+		conut++;
 		dataArray[i][2] = 0;
 		a++;
 		i++;
 	}
 	
 	perceptronTrainWeights(dataArray, learningParameter);
-	document.getElementById("weights-return").textContent=weights;
+	console.log(chart1data);
+	console.log(chart2data);
+	document.getElementById("weights-return").textContent = weights;
+	var ctx = document.getElementById("graph-2");
+	var myChart2 = new Chart(ctx, {
+		type: 'line',
+  		data: {
+  			datasets: [{
+  				label: "Perceptron Line",
+  				data: [{
+  					x: 0,
+    				y: (-bias/weights[1])
+				},{
+    				x: (-bias/weights[0]),
+    				y: 0
+  				}],
+  			}]
+  		}
+  	});
+	
 	console.log(weights);
 }
 
@@ -211,16 +241,16 @@ function start(){
 /* On clicking the Step button*/
 function changeStatusStep(){
 	document.getElementById("column-1").innerHTML = "iterations:2";
-	document.getElementById("step").style.visibility="hidden";
-	document.getElementById("step-100").style.visibility="hidden";
+	document.getElementById("step").style.visibility = "hidden";
+	document.getElementById("step-100").style.visibility = "hidden";
 }
 
 
 /* On clicking the Step100 button*/
 function changeStatusStep100(){
 	document.getElementById("column-2").innerHTML = "Classes Separated, iterations:2";
-	document.getElementById("step").style.visibility="hidden";
-	document.getElementById("step-100").style.visibility="hidden";
+	document.getElementById("step").style.visibility = "hidden";
+	document.getElementById("step-100").style.visibility = "hidden";
 }
 
 
@@ -228,20 +258,20 @@ function changeStatusStep100(){
 document.getElementById("clear").addEventListener("click", function() {
 	document.getElementById("column-1").innerHTML = "";
 	document.getElementById("column-2").innerHTML = "";
-	document.getElementById("input-data-from-user-class1").style.visibility="hidden";
-	document.getElementById("input-data-from-user-class2").style.visibility="hidden";
+	document.getElementById("input-data-from-user-class1").style.visibility = "hidden";
+	document.getElementById("input-data-from-user-class2").style.visibility = "hidden";
 	data1 = [];
 	data2 = [];
 	dataArray = [];
-	q1=0;
-	q2=0;
-	count1=0;
-	count2=0;
+	q1 = 0;
+	q2 = 0;
+	count1 = 0;
+	count2 = 0;
 	finaldata = [];
 	weights = [0, 0];
-	bias=1;
-	c=0;
-	d=0;
+	bias = 1;
+	c = 0;
+	d = 0;
 	activation = 0;
 	k = 0.05;
 	//window.myScatter.reset();
