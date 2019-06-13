83764da20ccd90d2f761d1bb082b4ba56cafa551
prekshap24 <rush2preksha@gmail.com>
2019-06-13 19:04:04 +0530

    basic layout 1

--- SRIP/Project-2 Issue Number 243/Codes/exp6.js
+++ SRIP/Project-2 Issue Number 243/Codes/exp6.js
@@ -6,10 +6,20 @@ var iterations=0;
 var row=[];
 var xVal;
 var yVal = 10;
+
 window.onload = function() {
     var dps=[]; //stores class 1 points and plots on chart
     var dps1 = []; //stores class 2 points and plots on chart
     var dps2 = [];
+    var xValClass1=[];
+    var yValClass1=[];
+    var xValClass2=[];
+    var yValClass2=[];
+    var xmeanClass1=0;
+    var ymeanClass1=0;
+    var xmeanClass2=0;
+    var ymeanClass2=0;
+
     var chart = new CanvasJS.Chart("chartContainer", {
         axisX:{
             title: "X-axis"
@@ -48,23 +58,23 @@ window.onload = function() {
     function addDataPointsAndRender1() {
         xValue = Number(document.getElementById("xValue").value);
         yValue = Number(document.getElementById("yValue").value);
-        /*var arr=[];
-        arr.push(xValue,yValue,1);
-        row.push(1);
-        data.push(arr);
-        arr=[];*/
+        xValClass1.push(xValue);
+        yValClass1.push(yValue);
+        
         dps.push({
             x: xValue,
             y: yValue
             });
         chart.render();
-        console.log(dps);
-                
+        console.log(xValClass1);
+        console.log(yValClass1);
     }
-
+    
     function addDataPointsAndRender2() {
         xValue = Number(document.getElementById("xValue").value);
         yValue = Number(document.getElementById("yValue").value);
+        xValClass2.push(xValue);
+        yValClass2.push(yValue);
         /*var arr=[];
         arr.push(xValue,yValue,1);
         row.push(0);
@@ -75,13 +85,38 @@ window.onload = function() {
             y: yValue
         });
         chart.render();
-        console.log(dps1);
+        console.log(xValClass2);
+        console.log(yValClass2);
     }
     
     function addcalculateMLE() {
-        
+        for(var i=0; i<xValClass1.length; i++){
+            xmeanClass1 = xmeanClass1 + xValClass1[i];
+        }
+        xmeanClass1 = xmeanClass1/xValClass1.length;
+
+        for(var i=0; i<yValClass1.length; i++){
+            ymeanClass1 = ymeanClass1 + yValClass1[i];
+        }
+        ymeanClass1 = ymeanClass1/yValClass1.length;
+
+        for(var i=0; i<xValClass2.length; i++){
+            xmeanClass2 = xmeanClass2 + xValClass2[i];
+        }
+        xmeanClass2 = xmeanClass2/xValClass2.length;
+
+        for(var i=0; i<yValClass2.length; i++){
+            ymeanClass2 = ymeanClass2 + yValClass2[i];
+        }
+        ymeanClass2 = ymeanClass2/yValClass2.length;
+
+        document.getElementById("mean1x").innerHTML=xmeanClass1;
+        document.getElementById("mean1y").innerHTML=ymeanClass1;
+        document.getElementById("mean2x").innerHTML=xmeanClass2;
+        document.getElementById("mean2y").innerHTML=ymeanClass2;
     }
     
+
     function addmarkAll() {
         
     }
