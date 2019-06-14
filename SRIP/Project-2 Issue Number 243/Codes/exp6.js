101c3fc5008c2faa65294d9f898c08181b208521
prekshap24 <rush2preksha@gmail.com>
2019-06-14 16:51:33 +0530

    algorithm added

--- SRIP/Project-2 Issue Number 243/Codes/exp6.js
+++ SRIP/Project-2 Issue Number 243/Codes/exp6.js
@@ -1,24 +1,27 @@
-var data1=[0,0,0];
-var data=[];
-var res=0;
-var learning_rate=0.001;
-var iterations=0;
-var row=[];
-var xVal;
-var yVal = 10;
-
 window.onload = function() {
-    var dps=[]; //stores class 1 points and plots on chart
-    var dps1 = []; //stores class 2 points and plots on chart
-    var dps2 = [];
-    var xValClass1=[];
-    var yValClass1=[];
-    var xValClass2=[];
-    var yValClass2=[];
-    var xmeanClass1=0;
-    var ymeanClass1=0;
-    var xmeanClass2=0;
-    var ymeanClass2=0;
+    var distributionFunction = 0;
+    var series1 = []; //stores class 1 points and plots on chart
+    var series2 = []; //stores class 2 points and plots on chart
+    var xmin_1 = 0;
+    var ymin_1 = 0;
+    var xmin_2 = 0;
+    var ymin_2 = 0;
+    var xmax_1 = 0;
+    var ymax_1 = 0;
+    var xmax_2 = 0;
+    var ymax_2 = 0;
+    var xmeanClass1 = 0;
+    var ymeanClass1 = 0;
+    var xmeanClass2 = 0;
+    var ymeanClass2 = 0;
+    var covariance11Val1 = 0;
+    var covariance12Val1 = 0;
+    var covariance22Val1 = 0;
+    var covariance11Val2 = 0;
+    var covariance12Val2 = 0;
+    var covariance22Val2 = 0;
+    var s1Size = 0;
+    var s2Size = 0;
 
     var chart = new CanvasJS.Chart("chartContainer", {
         axisX:{
@@ -32,99 +35,308 @@ window.onload = function() {
             legendText: "Class 1",
             color: "red",
             type: "scatter",
-            dataPoints: dps
+            dataPoints: series1
         },
         {
             showInLegend: true,           
             legendText: "Class 2",
             color: "blue",
             type: "scatter",
-            dataPoints: dps1
+            dataPoints: series2
             }]
         });
-
-    function addDataPoints() {
-        for(var j = 0; j < data.length; j++) { 
-            xVal = data[j][0];
-            yVal = (-data1[0]/data1[1])*xVal -(data1[2]/data1[1]);
-            dps2.push({
-                x: xVal,
-                y: yVal
-            });  
-            chart.render();    
-        }
-    }
 
     function addDataPointsAndRender1() {
         xValue = Number(document.getElementById("xValue").value);
         yValue = Number(document.getElementById("yValue").value);
-        xValClass1.push(xValue);
-        yValClass1.push(yValue);
-        
-        dps.push({
+        series1.push({
             x: xValue,
             y: yValue
             });
         chart.render();
-        console.log(xValClass1);
-        console.log(yValClass1);
     }
     
     function addDataPointsAndRender2() {
         xValue = Number(document.getElementById("xValue").value);
         yValue = Number(document.getElementById("yValue").value);
-        xValClass2.push(xValue);
-        yValClass2.push(yValue);
-        /*var arr=[];
-        arr.push(xValue,yValue,1);
-        row.push(0);
-        data.push(arr);
-        arr=[];*/
-        dps1.push({
+        series2.push({
             x: xValue,
             y: yValue
         });
         chart.render();
-        console.log(xValClass2);
-        console.log(yValClass2);
     }
     
+    function clear(){
+        series1=[]; 
+        series2 = []; 
+        xmeanClass1=0.0;
+        ymeanClass1=0.0;
+        xmeanClass2=0.0;
+        ymeanClass2=0.0;
+        chart.render();
+    }
+
     function addcalculateMLE() {
-        for(var i=0; i<xValClass1.length; i++){
-            xmeanClass1 = xmeanClass1 + xValClass1[i];
-        }
-        xmeanClass1 = xmeanClass1/xValClass1.length;
+        distributionFunction = document.getElementById("distribution-function").value;
+        var i1=0;
+        var i2=0;
+        xmin_1 = 100.0;
+        ymin_1 = 100.0;
+        xmin_2 = 100.0;
+        ymin_2 = 100.0;
+        xmax_1 = -100.0;
+        ymax_1 = -100.0;
+        xmax_2 = -100.0;
+        ymax_2 = -100.0;
+        xmeanClass1 = 0.0;
+        ymeanClass1 = 0.0;
+        xmeanClass2 = 0.0;
+        ymeanClass2 = 0.0;
+        var l1gammax = 0.0;
+        var l1gammay = 0.0;
 
-        for(var i=0; i<yValClass1.length; i++){
-            ymeanClass1 = ymeanClass1 + yValClass1[i];
+    //FINDING THE MEAN FOR X-VALUE AND Y-VALUE OF CLASS 1
+        for(i1 = 0; i1 < series1.length; i1++){
+            xmeanClass1 += series1[i1].x;
+            ymeanClass1 += series1[i1].y;
+        
+            if (distributionFunction == 1) {
+                l1gammax += series1[i1].x;
+                l1gammay += series1[i1].y;
+            }
+            if (series1[i1].x < xmin_1) {
+                xmin_1 = series1[i1].x;
+            }
+            if (series1[i1].x > xmax_1) {
+                xmax_1 = series1[i1].x;
+            }
+            if (series1[i1].y < ymin_1) {
+                ymin_1 = series1[i1].y;
+            }
+            if (!series1[i1].y > ymax_1) 
+                continue;
+            ymax_1 = series1[i1].y;
         }
-        ymeanClass1 = ymeanClass1/yValClass1.length;
 
-        for(var i=0; i<xValClass2.length; i++){
-            xmeanClass2 = xmeanClass2 + xValClass2[i];
+        if (distributionFunction == 1) {
+            l1gammax /= series1.length;
+            l1gammay /= series1.length;
         }
-        xmeanClass2 = xmeanClass2/xValClass2.length;
 
-        for(var i=0; i<yValClass2.length; i++){
-            ymeanClass2 = ymeanClass2 + yValClass2[i];
+        xmeanClass1 = xmeanClass1 / series1.length;
+        ymeanClass1 = ymeanClass1 / series1.length;
+        if (distributionFunction == 0) {
+            document.getElementById("mean1x").innerHTML = xmeanClass1;
+            document.getElementById("mean1y").innerHTML = ymeanClass1;
         }
-        ymeanClass2 = ymeanClass2/yValClass2.length;
 
-        document.getElementById("mean1x").innerHTML=xmeanClass1;
-        document.getElementById("mean1y").innerHTML=ymeanClass1;
-        document.getElementById("mean2x").innerHTML=xmeanClass2;
-        document.getElementById("mean2y").innerHTML=ymeanClass2;
-    }
+    //VARIANCE CALCULATION FOR X-VALUE AND Y-VALUE OF CLASS 1
+        covariance11Val1 = 0.0;
+        covariance12Val1 = 0.0;
+        covariance22Val1 = 0.0;
+        for (i1 = 0; i1 < series1.length; i1++) {
+            covariance11Val1 += Math.pow(series1[i1].x, 2.0);
+            covariance22Val1 += Math.pow(series1[i1].y, 2.0);
+            covariance12Val1 += series1[i1].y * series1[i1].x;
+        }
+
+        covariance11Val1 = covariance11Val1 / series1.length - xmeanClass1 * xmeanClass1;
+        covariance22Val1 = covariance22Val1 / series1.length - ymeanClass1 * ymeanClass1;
+        covariance12Val1 = covariance12Val1 / series1.length - xmeanClass1 * ymeanClass1;
+        
+        if (distributionFunction == 0) {
+            document.getElementById("c11-1").innerHTML = covariance11Val1;
+            document.getElementById("c12-1").innerHTML = covariance12Val1;
+            document.getElementById("c21-1").innerHTML = covariance12Val1;
+            document.getElementById("c22-1").innerHTML = covariance22Val1;
+        }
+
+        if (distributionFunction == 1) {
+            document.getElementById("c11-1").innerHTML = "";
+            document.getElementById("c12-1").innerHTML = "";
+            document.getElementById("c21-1").innerHTML = "";
+            document.getElementById("c22-1").innerHTML = "";
+        }
+        s1Size = series1.length;
+
+    //FINDING THE MEAN FOR X-VALUE AND Y-VALUE OF CLASS 2
+        var l2gammax = 0.0;
+        var l2gammay = 0.0;
+
+        for(i2 = 0; i2 < series2.length; i2++){
+            xmeanClass2 += series2[i2].x;
+            ymeanClass2 += series2[i2].y;
+        
+            if (distributionFunction == 1) {
+                l2gammax += series2[i2].x;
+                l2gammay += series2[i2].y;
+            }
+            if (series2[i2].x < xmin_2) {
+                xmin_2 = series2[i2].x;
+            }
+            if (series2[i2].x > xmax_2) {
+                xmax_2 = series2[i2].x;
+            }
+            if (series2[i2].y < ymin_2) {
+                ymin_2 = series2[i2].y;
+            }
+            if (!series2[i2].y > ymax_2)
+                continue;
+            ymax_2 = series2[i2].y;
+        }
+
+        if (distributionFunction == 1) {
+            l2gammax /= series2.length;
+            l2gammay /= series2.length;
+        }
+
+        xmeanClass2 = xmeanClass2 / series2.length;
+        ymeanClass2 = ymeanClass2 / series2.length;
+        if (distributionFunction == 0) {
+            document.getElementById("mean2x").innerHTML = xmeanClass2;
+            document.getElementById("mean2y").innerHTML = ymeanClass2;
+        }
+
+    //VARIANCE CALCULATION FOR X-VALUE AND Y-VALUE OF CLASS 2
+        covariance11Val2 = 0.0;
+        covariance12Val2 = 0.0;
+        covariance22Val2 = 0.0;
+        for (i2 = 0; i2 < series2.length; i2++) {
+            covariance11Val2 += Math.pow(series2[i2].x, 2.0);
+            covariance22Val2 += Math.pow(series2[i2].y, 2.0);
+            covariance12Val2 += series2[i2].y * series2[i2].x;
+        }
+
+        covariance11Val2 = covariance11Val2 / series2.length - xmeanClass2 * xmeanClass2;
+        covariance22Val2 = covariance22Val2 / series2.length - ymeanClass2 * ymeanClass2;
+        covariance12Val2 = covariance12Val2 / series2.length - xmeanClass2 * ymeanClass2;
+        
+        if (distributionFunction == 0) {
+            document.getElementById("c11-2").innerHTML = covariance11Val2;
+            document.getElementById("c12-2").innerHTML = covariance12Val2;
+            document.getElementById("c21-2").innerHTML = covariance12Val2;
+            document.getElementById("c22-2").innerHTML = covariance22Val2;
+        }
+
+        if (distributionFunction == 1) {
+            document.getElementById("c11-1").innerHTML = "";
+            document.getElementById("c12-1").innerHTML = "";
+            document.getElementById("c21-1").innerHTML = "";
+            document.getElementById("c22-1").innerHTML = "";
+            document.getElementById("c11-2").innerHTML = "";
+            document.getElementById("c12-2").innerHTML = "";
+            document.getElementById("c21-2").innerHTML = "";
+            document.getElementById("c22-2").innerHTML = "";
+        }
+        s2Size = series2.length;
+
+    }  
     
-
     function addmarkAll() {
+        var j;
+        var p1;
+        var p2;
+        var i;
+        var series1=[];
+        var series2 = [];
+        var l = -1;
+        var r = 1;
+        if(distributionFunction == 0){
+            for(i = l; i < r; i += (r - l) / 100.0){
+                for (j = l; j < r; j += (r - l) / 100.0) {
+                    p1 = prob1(j, i);
+                    if (p1 > (p2 = prob2(j, i))) {
+                        series1.push({
+                            x: i,
+                            y: j
+                        });
+                        chart.render();
+                        continue;
+                    }
+                    series2.push({
+                        x: i,
+                        y: j
+                    });
+                    chart.render();
+                }
+            }
+            //chart.render();
+        }
+
+        if (distributionFunction == 1) {
+            for (i = l; i < r; i += (r - l) / 100.0) {
+                for (j = l; j < r; j += (r - l) / 100.0) {
+                    p1 = prob1(j, i);
+                    if (p1 > (p2 = prob2(j, i))) {
+                        series1.push({
+                            x: i,
+                            y: j
+                        });
+                        chart.render();
+                    }
+                    if (p2 > p1) {
+                        series2.push({
+                            x: i,
+                            y: j
+                        });
+                        chart.render();
+                    }
+                    if (p1 != p2 || p1 == 0.0) 
+                        continue;
+                    series2.push({
+                        x: i,
+                        y: j
+                    });
+                    chart.render();
+                }
+            }
+            //chart.render();
+        }
+        console.log(series1);
+        console.log(series2);
         
     }
     
+    function prob1(x, y){
+        var r = 0.0;
+        if(distributionFunction == 0){
+            var sigmax = Math.sqrt(covariance22Val1);
+            var sigmay = Math.sqrt(covariance11Val1);
+            var rho = covariance12Val1 / (sigmax * sigmay);
+            rho = 0.0;
+            r = 1.0 / (6.283185307179586 * sigmax * sigmay * Math.sqrt(1.0 - rho * rho)) * Math.exp(-1.0 / (2.0 * (1.0 - rho * rho)) * (Math.pow(y - xmeanClass1, 2.0) / covariance11Val1 + Math.pow(x - ymeanClass1, 2.0) / covariance22Val1 - 2.0 * rho * (y - xmeanClass1) * (x - ymeanClass1) / (sigmax * sigmay)));
+            if (distributionFunction == 1 && x <= xmax_1 && x >= xmin_1 && y <= ymax_1 && y >= ymin_1) {
+                r = s1Size;
+            }
+            console.log(r);
+        return r;
+        }
+        
+    }
+
+    function prob2(x, y) {
+        var r = 0.0;
+        if (distributionFunction == 0) {
+            var sigmax = Math.sqrt(covariance22Val2);
+            var sigmay = Math.sqrt(covariance11Val2);
+            var rho = covariance12Val2 / (sigmax * sigmay);
+            rho = 0.0; 
+            r = 1.0 / (6.283185307179586 * sigmax * sigmay * Math.sqrt(1.0 - rho * rho)) * Math.exp(-1.0 / (2.0 * (1.0 - rho * rho)) * (Math.pow(y - xmeanClass2, 2.0) / covariance11Val2 + Math.pow(x - ymeanClass2, 2.0) / covariance22Val2 - 2.0 * rho * (y - xmeanClass2) * (x - ymeanClass2) / (sigmax * sigmay)));
+        }
+        if (distributionFunction == 1 && x <= xmax_2 && x >= xmin_2 && y <= ymax_2 && y >= ymin_2) {
+            r = s2Size;
+        }
+        console.log(r);
+        return r;
+    }
+
+
     var addClass1 = document.getElementById("add-class-1");
     addClass1.addEventListener("click", addDataPointsAndRender1);
     var addClass2 = document.getElementById("add-class-2");
     addClass2.addEventListener("click", addDataPointsAndRender2);
+    var clearButton = document.getElementById("clear");
+    clearButton.addEventListener("click", clear)
     var calculateMLE = document.getElementById("calculate-mle");
     calculateMLE.addEventListener("click", addcalculateMLE);
     var markAll=document.getElementById("mark-all");
