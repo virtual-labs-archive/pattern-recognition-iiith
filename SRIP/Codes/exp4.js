3911284b8cdf28cd0f4b8674ced79ef39ec0cc43
prekshap24 <rush2preksha@gmail.com>
2019-06-25 14:23:02 +0530

    4 datasets added

--- SRIP/Codes/exp4.js
+++ SRIP/Codes/exp4.js
@@ -1,25 +1,25 @@
 window.onload = function() {
 	var loadDataset = 0;
 //first dataset
-	var series11 = [{x: 0.3, y: 0.2}, {x: 0.4, y: 0.3}];
-	var series12 = [{x: -0.4, y: -0.3}, {x: -0.3, y: -0.2}];
-	var series13 = [{x: 0.2, y: -0.4}, {x: 0.3, y: -0.2}];
-	var series14 = [{x: -0.4, y: 0.6}, {x: -0.7, y: 0.3}];
+	var series11 = [{x: 1, y: 3}, {x: 2, y: 2}];
+	var series12 = [{x: 4, y: 10}, {x: 5, y: 10.5}];
+	var series13 = [{x: 7, y: 6}, {x: 7, y: 7}];
+	var series14 = [{x: 10, y: 1}, {x: 11, y: 1}];
 //second dataset
-	var series21 = [];
-	var series22 = [];
-	var series23 = [];
-	var series24 = [];
+	var series21 = [{x: 3, y: 4}, {x: 2, y: 3}];
+	var series22 = [{x: 5, y: -2}, {x: 4, y: -3}];
+	var series23 = [{x: -2, y: -4}, {x: -3, y: -4}];
+	var series24 = [{x: -4, y: 2}, {x: -5, y: 2}];
 //third dataset
-    var series31 = [];
-    var series32 = [];
-    var series33 = [];
-    var series34 = [];
+    var series31 = [{x: 0.3, y: 0.2}, {x: 0.4, y: 0.3}];
+    var series32 = [{x: -0.4, y: -0.3}, {x: -0.3, y: -0.2}];
+    var series33 = [{x: 0.2, y: -0.4}, {x: 0.3, y: -0.2}];
+    var series34 = [{x: -0.4, y: 0.6}, {x: -0.7, y: 0.3}];
 //fourth dataset
-    var series41 = [];
-    var series42 = [];
-    var series43 = [];
-    var series44 = [];
+    var series41 = [{x: -13, y: -7}, {x: -12, y: -5}];
+    var series42 = [{x: -2, y: -1}, {x: -3, y: -2}];
+    var series43 = [{x: -2, y: -17}, {x: -1, y: -19}];
+    var series44 = [{x: -17, y: -15}, {x: -13, y: -16}];
 	
 	var ans = [];
 	var ddag1;
@@ -80,23 +80,29 @@ window.onload = function() {
         	chart.render();
         }
 
-        /*if(loadDataset == 2){
+        if(loadDataset == 2){
             chart.options.data[0].dataPoints = series21;
             chart.options.data[1].dataPoints = series22;
+            chart.options.data[2].dataPoints = series23;
+            chart.options.data[3].dataPoints = series24;
             chart.render();
         }
 
         if(loadDataset == 3){
             chart.options.data[0].dataPoints = series31;
             chart.options.data[1].dataPoints = series32;
+            chart.options.data[2].dataPoints = series33;
+            chart.options.data[3].dataPoints = series34;
             chart.render();
         }
 
         if(loadDataset == 4){
             chart.options.data[0].dataPoints = series41;
             chart.options.data[1].dataPoints = series42;
+            chart.options.data[2].dataPoints = series43;
+            chart.options.data[3].dataPoints = series44;
             chart.render();
-        }*/
+        }
     }
 
 	function addDataPointsAndRender() {
@@ -171,15 +177,189 @@ window.onload = function() {
                 return 1;
             }  
         }
-        /*if(loadDataset === 2){
-            
+
+//second dataset
+        if(loadDataset == 2){
+            var i = 0;
+            var j = 0;
+            var a1 = 0;
+            var b1 = 0;
+            var c1 = 0;
+            var a2 = 0;
+            var b2 = 0;
+            var c2 = 0;
+            var a3 = 0;
+            var b3 = 0;
+            var c3 = 0;
+            var a4 = 0;
+            var b4 = 0;
+            var c4 = 0;
+            for(i = 0; i<2; i++){
+                for(j = 0; j < ans.length; j++){
+                    a1 = series21[i].x - ans[j].x;
+                    b1 = series21[i].y - ans[j].y;
+                    c1 += Math.sqrt((a1 * a1) + (b1 * b1));
+                }
+            }
+            for(i = 0; i<2; i++){
+                for(j = 0; j < ans.length; j++){
+                    a2 = series22[i].x - ans[j].x;
+                    b2 = series22[i].y - ans[j].y;
+                    c2 += Math.sqrt((a2 * a2) + (b2 * b2));
+                }            
+            }
+            for(i = 0; i<2; i++){
+                for(j = 0; j < ans.length; j++){
+                    a3 = series23[i].x - ans[j].x;
+                    b3 = series23[i].y - ans[j].y;
+                    c3 += Math.sqrt((a3 * a3) + (b3 * b3));
+                }            
+            }
+            for(i = 0; i<2; i++){
+                for(j = 0; j < ans.length; j++){
+                    a4 = series24[i].x - ans[j].x;
+                    b4 = series24[i].y - ans[j].y;
+                    c4 += Math.sqrt((a4 * a4) + (b4 * b4));
+                }            
+            }
+            if(c1 < c2 && c1 < c3 && c1 < c4){
+                return 1;
+            }
+            else if(c2 < c1 && c2 < c3 && c2 < c4){
+                return 2;
+            }
+            else if(c3 < c1 && c3 < c2 && c3 < c4){
+                return 3;
+            }
+            else if(c4 < c1 && c4 < c3 && c4 < c3){
+                return 4;
+            }
+            else{
+                return 1;
+            }
         }
-        if(loadDataset === 3){
-            
+
+//third dataset
+        if(loadDataset == 3){
+            var i = 0;
+            var j = 0;
+            var a1 = 0;
+            var b1 = 0;
+            var c1 = 0;
+            var a2 = 0;
+            var b2 = 0;
+            var c2 = 0;
+            var a3 = 0;
+            var b3 = 0;
+            var c3 = 0;
+            var a4 = 0;
+            var b4 = 0;
+            var c4 = 0;
+            for(i = 0; i<2; i++){
+                for(j = 0; j < ans.length; j++){
+                    a1 = series31[i].x - ans[j].x;
+                    b1 = series31[i].y - ans[j].y;
+                    c1 += Math.sqrt((a1 * a1) + (b1 * b1));
+                }
+            }
+            for(i = 0; i<2; i++){
+                for(j = 0; j < ans.length; j++){
+                    a2 = series32[i].x - ans[j].x;
+                    b2 = series32[i].y - ans[j].y;
+                    c2 += Math.sqrt((a2 * a2) + (b2 * b2));
+                }            
+            }
+            for(i = 0; i<2; i++){
+                for(j = 0; j < ans.length; j++){
+                    a3 = series33[i].x - ans[j].x;
+                    b3 = series33[i].y - ans[j].y;
+                    c3 += Math.sqrt((a3 * a3) + (b3 * b3));
+                }            
+            }
+            for(i = 0; i<2; i++){
+                for(j = 0; j < ans.length; j++){
+                    a4 = series34[i].x - ans[j].x;
+                    b4 = series34[i].y - ans[j].y;
+                    c4 += Math.sqrt((a4 * a4) + (b4 * b4));
+                }            
+            }
+            if(c1 < c2 && c1 < c3 && c1 < c4){
+                return 1;
+            }
+            else if(c2 < c1 && c2 < c3 && c2 < c4){
+                return 2;
+            }
+            else if(c3 < c1 && c3 < c2 && c3 < c4){
+                return 3;
+            }
+            else if(c4 < c1 && c4 < c3 && c4 < c3){
+                return 4;
+            }
+            else{
+                return 1;
+            }
         }
-        if(loadDataset === 4){
-            
-        }*/
+
+//fourth dataset
+        if(loadDataset == 4){
+            var i = 0;
+            var j = 0;
+            var a1 = 0;
+            var b1 = 0;
+            var c1 = 0;
+            var a2 = 0;
+            var b2 = 0;
+            var c2 = 0;
+            var a3 = 0;
+            var b3 = 0;
+            var c3 = 0;
+            var a4 = 0;
+            var b4 = 0;
+            var c4 = 0;
+            for(i = 0; i<2; i++){
+                for(j = 0; j < ans.length; j++){
+                    a1 = series41[i].x - ans[j].x;
+                    b1 = series41[i].y - ans[j].y;
+                    c1 += Math.sqrt((a1 * a1) + (b1 * b1));
+                }
+            }
+            for(i = 0; i<2; i++){
+                for(j = 0; j < ans.length; j++){
+                    a2 = series42[i].x - ans[j].x;
+                    b2 = series42[i].y - ans[j].y;
+                    c2 += Math.sqrt((a2 * a2) + (b2 * b2));
+                }            
+            }
+            for(i = 0; i<2; i++){
+                for(j = 0; j < ans.length; j++){
+                    a3 = series43[i].x - ans[j].x;
+                    b3 = series43[i].y - ans[j].y;
+                    c3 += Math.sqrt((a3 * a3) + (b3 * b3));
+                }            
+            }
+            for(i = 0; i<2; i++){
+                for(j = 0; j < ans.length; j++){
+                    a4 = series44[i].x - ans[j].x;
+                    b4 = series44[i].y - ans[j].y;
+                    c4 += Math.sqrt((a4 * a4) + (b4 * b4));
+                }            
+            }
+            if(c1 < c2 && c1 < c3 && c1 < c4){
+                return 1;
+            }
+            else if(c2 < c1 && c2 < c3 && c2 < c4){
+                return 2;
+            }
+            else if(c3 < c1 && c3 < c2 && c3 < c4){
+                return 3;
+            }
+            else if(c4 < c1 && c4 < c3 && c4 < c3){
+                return 4;
+            }
+            else{
+                return 1;
+            }
+        }
     }
 
     function startDDAG(){
