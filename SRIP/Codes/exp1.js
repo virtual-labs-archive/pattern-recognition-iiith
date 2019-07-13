4de47510413b5dec2051c5cfbe29f92a87d09aa2
prekshap24 <rush2preksha@gmail.com>
2019-07-13 16:31:07 +0530

    code addition

--- SRIP/Codes/exp1.js
+++ SRIP/Codes/exp1.js
@@ -1,6 +1,12 @@
 window.onload = function() {
-    var series1 = []; //stores class 1 points and plots on chart
-    var series2 = []; //stores class 2 points and plots on chart
+    var t1dataset = [["Class", "ImageID", "Contrast Ratio", "Aspect Ratio", "Perimeter", "X-Symmetry", "Y-Symmetry", "Holes"], 
+    [0, 2, 0.19, 0.092, 87, 131, 137, 2],
+    [0, 22, 0.204, 0.095, 86, 143, 133, 2],
+    [0, 35, 0.215505761659127, 0.0847880299251871, 77.0, 113.0, 119.0, 2.0], 
+    [0, 38, 0.236594959162299, 0.0897755610972568, 73.0, 132.0, 130.0, 2.0],
+    [0, 52, 0.278957873167097, 0.0972568578553617, 93.0, 149.0, 125.0, 2.0]];
+    var series1 = [];
+    var series2 = [];
     var dataset = 0;
     var chart = new CanvasJS.Chart("chartContainer", {
         title: {text: "Chart"},
@@ -33,11 +39,37 @@ window.onload = function() {
         document.getElementById("plot-button").disabled = false;
         document.getElementById("test-button").disabled = false;
         document.getElementById("target").textContent = "Target Accuracy: " + dataset;
+        document.getElementById("table-id").style.visibility = "visible";
+        var table = document.createElement("TABLE");
+        table.border = "1";
+        table.width="100%";
+ 
+        //Get the count of columns.
+        var columnCount = t1dataset[0].length;
+ 
+        //Add the header row.
+        var row = table.insertRow(-1);
+        for (var i = 0; i < columnCount; i++) {
+            var headerCell = document.createElement("TH");
+            headerCell.innerHTML = t1dataset[0][i];
+            row.appendChild(headerCell);
+        }
+ 
+        //Add the data rows.
+        for (var i = 1; i < t1dataset.length; i++) {
+            row = table.insertRow(-1);
+            for (var j = 0; j < columnCount; j++) {
+                var cell = row.insertCell(-1);
+                cell.innerHTML = t1dataset[i][j];
+            }
+        }
+ 
+        var dvTable = document.getElementById("table-id");
+        dvTable.textContent = "";
+        dvTable.appendChild(table);
     }
 
     function plot(){
-        var xValue = 1;
-        var yValue = 2;
         series1.push({
             x: xValue,
             y: yValue
@@ -49,14 +81,4 @@ window.onload = function() {
     lo.addEventListener("click", load);
     var plo = document.getElementById("plot-button");
     plo.addEventListener("click", plot);
-    /*var addClass2 = document.getElementById("add-class-2");
-    addClass2.addEventListener("click", addDataPointsAndRender2);
-    var addClass1 = document.getElementById("add-class-1");
-    addClass1.addEventListener("click", addDataPointsAndRender1);
-    var addClass2 = document.getElementById("add-class-2");
-    addClass2.addEventListener("click", addDataPointsAndRender2);
-    var addClass1 = document.getElementById("add-class-1");
-    addClass1.addEventListener("click", addDataPointsAndRender1);
-    var addClass2 = document.getElementById("add-class-2");
-    addClass2.addEventListener("click", addDataPointsAndRender2);*/
 };
