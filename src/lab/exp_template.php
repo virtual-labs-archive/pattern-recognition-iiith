d803f2d27fc2156dac289772fd22914eca7aa0bd
mrudhvika940 <mrudhvikadamaraju@gmail.com>
2020-03-31 15:47:38 +0530

    added header and footer for all experiments

--- src/lab/exp_template.php
+++ src/lab/exp_template.php
@@ -2,6 +2,8 @@
 <html lang="en" class="no-js">
 
 <head>
+<script class='gtm'>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W59SWTR');</script>
+
   
   <!--It is a good practice to declare the encoding even for an 
   English Web site. One function of this is to tag is to "reset"
